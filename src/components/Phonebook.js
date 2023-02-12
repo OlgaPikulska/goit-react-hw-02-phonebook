d667905d9141d765d0e2e17c242b2b941d2c3da7
Olga Pikulska <olga.pikulska.ld@gmail.com>
2023-02-12 16:09:45 +0100

    Add input number

--- src/components/Phonebook.js
+++ src/components/Phonebook.js
@@ -26,6 +26,7 @@ border-radius: 3px;
 `
 const INITIAL_STATE = {
     name: "",
+    number: "",
 }
 export class Phonebook extends Component {
     state = { ...INITIAL_STATE };
@@ -60,6 +61,17 @@ export class Phonebook extends Component {
                         name="name"
                         pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
                         title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                        required
+                        onChange={this.handleChange}
+                    /></StyledLabel>
+
+                <StyledLabel>
+                    Number
+                    <StyledInput
+                        type="tel"
+                        name="number"
+                        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+                        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
                         required
                         onChange={this.handleChange}
                     /></StyledLabel>
